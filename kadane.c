76ff17e30fdcbea0b0f17b53097215e28ae1e4f5
Manas173 <manasranjanswain173@gmail.com>
2019-10-21 17:24:23 +0530

    Added maximum subarray sum (Kadane's Algorithm) [with changes]

--- kadane.c
+++ kadane.c
@@ -1,5 +1,31 @@
-#include <stdio.h>
+/*
+This code has been written by Manas Ranjan Swain (https://github.com/Manas173)
 
+Description: To find the maximum sum of the contiguous subarray and to print the value.
+For Example:
+
+In array having elements as:
+-10  5 -1 -8 12 15 2 -100 101 -100
+
+The answer would be 101 as this would be the maximum sum of a contiguous subarray (only one element in this case).
+    
+INPUT:
+First line will contain the number of elements i.e, the size of the array
+Next N lines will contain N array elements.
+
+OUTPUT:
+A single line output that contains the maximum sum of the contigous subarray
+
+TESTCASE:
+
+8                                                                                                                                                                                     
+Enter 8 elements 2 -1 10 -12 -1 5 2 -3                                                                                                                                                 
+The max sum of the subarray is 11
+
+EXPLANATION: The maximum sum contiguous subarray is from 1st element to 3rd element
+*/
+
+#include <stdio.h>
  
 int maximumSubArraySum(int arr[], int n)
 {
@@ -18,14 +44,13 @@ int maximumSubArraySum(int arr[], int n)
  
 int main()
 {
-    int arr[] = {-10 , 5 ,-1 , -8 ,12, 15,2 ,-100 , 101 ,-100};
-    int size = sizeof(arr) / sizeof(arr[0]);
- 
-    printf(" The max sum of the subarray is %d", maximumSubArraySum(arr, size));
-    
-    int arr2[] = { -20, -2, 1 ,4 ,5 ,-3, 7, -25};
-    int size2 = sizeof(arr2) / sizeof(arr2[0]);
-    
-    printf("\n The max sum of the subarray is %d", maximumSubArraySum(arr2, size2));
+    int size;
+    scanf("%d",&size);
+    printf("Enter %d elements ",size);
+    int arr[size];
+    for(int i=0;i<size;i++)
+        scanf("%d",&arr[i]);
+
+    printf("The max sum of the subarray is %d", maximumSubArraySum(arr, size));
     return 0;
 }
